d101206070a436c1ee760a795e0b3ffeead0ccc8
richard <richardlucas761@protonmail.com>
2026-01-14 16:16:46 +0000

    Finishing AlertService tests.

--- AlertService/AlertServiceTests/AlertServiceTest.cs
+++ AlertService/AlertServiceTests/AlertServiceTest.cs
@@ -1,28 +1,51 @@
 ﻿using AlertService;
+using FluentAssertions;
 using Moq;
 
 namespace AlertServiceTests
 {
     public class AlertServiceTest
     {
-        //private readonly IAlertDao alertDao = new Mock<IAlertDao>();
+        private readonly Mock<IAlertDao> _alertDaoMock = new();
 
         private readonly AlertService.AlertService alertService;
 
-        public AlertServiceTest(IAlertDao alertDao)
+        private readonly Guid Guid1 = Guid.NewGuid();
+        private readonly Guid Guid2 = Guid.NewGuid();
+
+        private readonly DateTime AlertTime = DateTime.UtcNow.AddDays(-1);
+
+        public AlertServiceTest()
         {
-            //alertService = new AlertService.AlertService(alertDao.Object());
+            alertService = new AlertService.AlertService(_alertDaoMock.Object);
         }
 
         [TestCase]
         public void RaiseAlert()
         {
             // Arrange
+            _alertDaoMock.Setup(x => x.AddAlert(It.IsAny<DateTime>())).Returns(Guid1);
 
             // Act
-            alertService.RaiseAlert();
+            var response = alertService.RaiseAlert();
 
             // Assert
+            response.Should().Be(Guid1);
+            _alertDaoMock.Verify(x => x.AddAlert(It.IsAny<DateTime>()), Times.Once);
+        }
+
+        [TestCase]
+        public void GetAlertTime()
+        {
+            // Arrange
+            _alertDaoMock.Setup(x => x.GetAlert(Guid2)).Returns(AlertTime);
+
+            // Act
+            var response = alertService.GetAlertTime(Guid2);
+
+            // Assert
+            response.Should().Be(AlertTime);
+            _alertDaoMock.Verify(x => x.GetAlert(Guid2), Times.Once);
         }
     }
 }
